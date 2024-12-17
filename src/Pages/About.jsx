82872037fd431e380f99d7c76e289edf6eac9d33
saulyez <saulye25@gmail.com>
2024-12-17 12:11:16 +0000

    Initial commit

---
+++ src/Pages/About.jsx
@@ -0,0 +1,2 @@
+const About = () => <div><h1>About</h1></div>;
+export default About;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,2 @@
+const Home = () => <div><h1>Home</h1><p>Welcome to my portfolio!</p></div>;
+export default Home;
