b0818eebf4148c60e1bc5a3623991eff2a07ce85
saulyez <saulye25@gmail.com>
2024-12-17 19:55:54 +0000

    Advanced the Home page and navbar

--- src/Components/forLoopanimation.jsx
+++ src/Components/forLoopanimation.jsx
@@ -1,14 +1,13 @@
+import React from 'react';
 import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
-const animationFor = () => {
-    return (
-      <DotLottieReact
-        src="https://lottie.host/1db069de-ad65-408d-a0ad-2f64d773bfee/T8PUiLRPyi.lottie"
-        loop
-        autoplay
-      />
-    );
-  };
-  
-  export default animationFor
-  
+const Animation = () => {
+  return (
+    <DotLottieReact
+      src="https://lottie.host/1db069de-ad65-408d-a0ad-2f64d773bfee/T8PUiLRPyi.lottie"
+      loop
+      autoplay
+    />
+  );
+};
+export default Animation;
