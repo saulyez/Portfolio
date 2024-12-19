ef7cf740f49b8b82128b9abdb7a3154ac9cec05a
saulyez <saulye25@gmail.com>
2024-12-19 16:58:20 +0000

    Began the about page. Modified the footer

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,2 +1,16 @@
-const About = () => <div><h1>About</h1></div>;
+import '../Styles/About.css';
+const About = () => {
+    return (
+        <div className="About-Container">
+            <div className='About-Content'>
+                <h1>About Me</h1>
+                <p>My name is Saul and I come San Jose, Costa Rica. I am passionate about artificial intelligence and programming. I aspire to become a software engineer and build meaningful projects. Pura Vida 🇨🇷</p>
+            </div>
+            {/* <div className='Carousel-Container'>
+                <CarouselImg />
+            </div> */}
+
+        </div>
+    );
+}
 export default About;
