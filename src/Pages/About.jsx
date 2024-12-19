dacf4c52fc1c96826be4491203a1fc1b0a5fac10
saulyez <saulye25@gmail.com>
2024-12-19 21:37:55 +0000

    Created the schools slider

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,16 +1,22 @@
 import '../Styles/About.css';
+import ImageSlider from '../Components/Schools';
+
 const About = () => {
     return (
         <div className="About-Container">
-            <div className='About-Content'>
+            <div className="About-Content">
                 <h1>About Me</h1>
-                <p>My name is Saul and I come San Jose, Costa Rica. I am passionate about artificial intelligence and programming. I aspire to become a software engineer and build meaningful projects. Pura Vida 🇨🇷</p>
+                <p>
+                    My name is Saul and I come from San Jose, Costa Rica. I am passionate about artificial intelligence and programming. 
+                    I aspire to become a software engineer and build meaningful projects. Pura Vida 🇨🇷
+                </p>
             </div>
-            {/* <div className='Carousel-Container'>
-                <CarouselImg />
-            </div> */}
 
+            <div className="Schools-Container">
+                <ImageSlider />
+            </div>
         </div>
     );
-}
+};
+
 export default About;
